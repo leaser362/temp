02045b55e730aff8e89244ca1ce93fe11a2f47ae
Vo Minh Khang <khangpro590@gmail.com>
2021-10-15 17:20:27 +0700

    Create bai4cham11.c

---
+++ C/bai4cham11.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+bool 
+
+void TaoMang(int arr[],int n){
+    srand((int)time(0));
+    int i;
+    for(i=0;i<n;i++){
+        
+    }
+}
+
+void InMang(int arr[],int n){
+    int i;
+    for(i=0;i<n;i++){
+        printf(" %d ",arr[i]);
+    }
+
+int main(){
+    srand((int)time(0));
+    int n=0;
+    while (n<0)
+    {
+        printf("Nhap N:");
+        printf("%d",&n);
+    }
+    TaoMang(arr,n); 
+    InMang(arr,n);
+    
+    return 0;
+}
